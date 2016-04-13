fa0c354b6b16f7b884a2a5067499e10675b078b7
Sam Krause <samuelnkrause@gmail.com>
2016-04-13 19:59:52 -0400

    created method to popluate create  playercards with top 300 players.

--- app/models/cbs.rb
+++ app/models/cbs.rb
@@ -2,9 +2,11 @@ class CBS
   def self.cbsCreatePlayercards
     rankings = JSON.parse(playerRanking())['body']['rankings']['positions']
     rankings.each do |position|
-      if position['abbr'] = ("C" or "1B" or "2B" or "3B" or "SS" or "OF")
+      if position.has_value?("C") or position.has_value?("1B") or position.has_value?("2B") or position.has_value?("3B") or position.has_value?("SS") or position.has_value?("OF")
+        puts position['abbr']
         position['players'].each do |player|
           puts player['rank'].to_s + ' ' + player['firstname'] + ' ' + player['lastname'] + ' ' + position['abbr'] + ' ' + player['id']
+          Playercard.create(first_name: player['firstname'], last_name: player['lastname'], position: position['abbr'], rank: player['rank'], cbs_id: player['id'].to_i)
         end
       end
     end
